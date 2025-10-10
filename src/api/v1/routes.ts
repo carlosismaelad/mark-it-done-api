ef819dd7f385745b4ef094fe1ee419aafe83f2f1
Carlos Dourado <carlosismaelad@outlook.com>
2025-10-10 15:04:45 -0300

    feat: register migrations routes

--- src/api/v1/routes.ts
+++ src/api/v1/routes.ts
@@ -2,9 +2,11 @@ import { FastifyInstance } from "fastify";
 import { usersRoutes } from "./user/routes.js";
 import { itemsRoutes } from "./item/routes.js";
 import { statusRoutes } from "./status/routes.js";
+import { migrationsRoutes } from "./migrations/routes.js";
 
 export async function apiV1Routes(fastify: FastifyInstance) {
   await fastify.register(statusRoutes, { prefix: "/status" });
+  await fastify.register(migrationsRoutes, { prefix: "/migrations" });
   await fastify.register(usersRoutes, { prefix: "/users" });
   await fastify.register(itemsRoutes, { prefix: "/items" });
 }
