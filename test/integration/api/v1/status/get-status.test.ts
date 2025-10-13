0812eb3ec4a47ec7752000cea58d3cbd6d19d427
Carlos Dourado <carlosismaelad@outlook.com>
2025-10-13 16:07:17 -0300

    fix: fixes the number of expected connections in the test

--- test/integration/api/v1/status/get-status.test.ts
+++ test/integration/api/v1/status/get-status.test.ts
@@ -19,7 +19,7 @@ describe("GET /api/v1/status", () => {
 
       expect(responseBody.dependencies.database.version).toEqual("16.0");
       expect(responseBody.dependencies.database.max_connections).toEqual(100);
-      expect(responseBody.dependencies.database.opened_connections).toEqual(2);
+      expect(responseBody.dependencies.database.opened_connections).toEqual(1);
     });
   });
 });
