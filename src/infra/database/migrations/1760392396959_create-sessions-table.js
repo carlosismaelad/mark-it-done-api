cd952c16e879e7c280ba4230478e0e53ff4b20de
Carlos Dourado <carlosismaelad@outlook.com>
2025-10-14 20:47:35 -0300

    build: adds new columsn to sessions migration

--- src/infra/database/migrations/1760392396959_create-sessions-table.js
+++ src/infra/database/migrations/1760392396959_create-sessions-table.js
@@ -17,16 +17,34 @@ exports.up = (pgm) => {
     expires_at: {
       type: "timestamptz",
       notNull: true,
-    },
-    created_at: {
-      type: "timestamptz",
-      notNull: true,
-      default: pgm.func("timezone('utc', now())"),
-    },
-    updated_at: {
-      type: "timestamptz",
-      notNull: true,
-      default: pgm.func("timezone('utc', now())"),
+      created_at: {
+        type: "timestamptz",
+        notNull: true,
+        default: pgm.func("timezone('utc', now())"),
+      },
+      updated_at: {
+        type: "timestamptz",
+        notNull: true,
+        default: pgm.func("timezone('utc', now())"),
+      },
+      code: {
+        type: "varchar(6)",
+        notNull: true,
+      },
+      attempts: {
+        type: "integer",
+        notNull: true,
+        default: 0,
+      },
+      code_expires_at: {
+        type: "timestamp",
+        notNull: true,
+      },
+      status: {
+        type: "varchar(20)",
+        notNull: true,
+        default: "pending",
+      },
     },
   });
 };
