a7c4719438c4f9b50d090ba579012ba44424c5b5
Carlos Dourado <carlosismaelad@outlook.com>
2025-10-15 14:59:18 -0300

    fix: fixes sessions migration

--- src/infra/database/migrations/1760392396959_create-sessions-table.js
+++ src/infra/database/migrations/1760392396959_create-sessions-table.js
@@ -1,4 +1,4 @@
-exports.up = (pgm) => {
+export const up = (pgm) => {
   pgm.createTable("sessions", {
     id: {
       type: "uuid",
@@ -17,36 +17,36 @@ exports.up = (pgm) => {
     expires_at: {
       type: "timestamptz",
       notNull: true,
-      created_at: {
-        type: "timestamptz",
-        notNull: true,
-        default: pgm.func("timezone('utc', now())"),
-      },
-      updated_at: {
-        type: "timestamptz",
-        notNull: true,
-        default: pgm.func("timezone('utc', now())"),
-      },
-      code: {
-        type: "varchar(6)",
-        notNull: true,
-      },
-      attempts: {
-        type: "integer",
-        notNull: true,
-        default: 0,
-      },
-      code_expires_at: {
-        type: "timestamp",
-        notNull: true,
-      },
-      status: {
-        type: "varchar(20)",
-        notNull: true,
-        default: "pending",
-      },
+    },
+    created_at: {
+      type: "timestamptz",
+      notNull: true,
+      default: pgm.func("timezone('utc', now())"),
+    },
+    updated_at: {
+      type: "timestamptz",
+      notNull: true,
+      default: pgm.func("timezone('utc', now())"),
+    },
+    code: {
+      type: "varchar(6)",
+      notNull: true,
+    },
+    attempts: {
+      type: "integer",
+      notNull: true,
+      default: 0,
+    },
+    code_expires_at: {
+      type: "timestamptz",
+      notNull: true,
+    },
+    status: {
+      type: "varchar(20)",
+      notNull: true,
+      default: "pending",
     },
   });
 };
 
-exports.down = false;
+export const down = false;
