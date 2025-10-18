646ed444684ba44d18dbe2f3356d3e337f1dd0b1
Carlos Dourado <carlosismaelad@outlook.com>
2025-10-18 20:12:20 -0300

    test: change jest settings for project compatibility

--- jest.config.js
+++ jest.config.js
@@ -4,18 +4,25 @@ export default {
   extensionsToTreatAsEsm: [".ts"],
   testEnvironment: "node",
 
-  // Where to look for tests (in your test/ folder)
-  testMatch: ["<rootDir>/test/**/*.test.ts"],
+  // Load environment variables for tests
+  setupFiles: ["<rootDir>/tests/setup.ts"],
+
+  // Where to look for tests (in your tests/ folder)
+  testMatch: ["<rootDir>/tests/**/*.test.ts"],
+
+  // Transform ES Modules from node_modules
+  transformIgnorePatterns: ["node_modules/(?!(uuid|@faker-js/faker|set-cookie-parser|node-pg-migrate)/)"],
 
   // Mock problematic ES Modules
   moduleNameMapper: {
     "^(\\.{1,2}/.*)\\.js$": "$1",
-    "^node-pg-migrate$": "<rootDir>/test/__mocks__/node-pg-migrate.js",
+    "^@/(.*)$": "<rootDir>/src/$1",
+    "^../src/api/v1/migrations/service/migrations-service$": "<rootDir>/tests/__mocks__/migrations-service-mock.ts",
   },
 
   // How to transform TypeScript files
   transform: {
-    "^.+\\.ts$": [
+    "^.+\\.(ts|js)$": [
       "ts-jest",
       {
         useESM: true,
